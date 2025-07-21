f28414b70b5ff75a28a79e5b5306237f376c1b1e
JAEIK JEONG <wodlr1207@naver.com>
2025-07-21 20:20:47 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 592 ms, Memory: 142452 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static List<Integer>[] graph;
+    static boolean[] visited;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    static void dfs(int current) {
+        visited[current] = true;
+
+        for (int next : graph[current]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}

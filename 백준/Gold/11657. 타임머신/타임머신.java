1f3980758f46c87990896a9f438ffb52b3f317dc
JAEIK JEONG <wodlr1207@naver.com>
2025-08-22 22:15:46 +0900

    [Gold IV] Title: 타임머신, Time: 288 ms, Memory: 19452 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -8,8 +8,8 @@ import java.util.StringTokenizer;
 
 // 골드 4 벨만포드
 public class Main {
-    static List<List<Edge>> graph = new ArrayList<>();
-    static long[] dist; 
+    static List<Edge> edges = new ArrayList<>();
+    static long[] dist;
     static int cityTotal;
     static int busTotal;
 
@@ -37,41 +37,36 @@ public class Main {
             dist[i] = Long.MAX_VALUE;
         }
 
-        for (int i = 0; i <= cityTotal; i++) {
-            graph.add(new ArrayList<>());
-        }
-
         for (int i = 0; i < busTotal; i++) {
             st = new StringTokenizer(br.readLine());
             int startCity = Integer.parseInt(st.nextToken());
             int endCity = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            graph.get(startCity).add(new Edge(startCity, endCity, cost));
+            edges.add(new Edge(startCity, endCity, cost));
         }
 
         dist[1] = 0;
 
+        // 벨만포드 (최단거리 구하기)
         for (int i = 1; i < cityTotal; i++) {
-            for (int j = 1; j <= cityTotal; j++) {
-                for (Edge edge : graph.get(j)) {
-                    if (dist[edge.from] != Long.MAX_VALUE &&
-                            dist[edge.to] > dist[edge.from] + edge.cost) {
-                        dist[edge.to] = dist[edge.from] + edge.cost;
-                    }
-                }
-            }
-        }
-
-        for (int j = 1; j <= cityTotal; j++) {
-            for (Edge edge : graph.get(j)) {
+            for (Edge edge : edges) {
                 if (dist[edge.from] != Long.MAX_VALUE &&
                         dist[edge.to] > dist[edge.from] + edge.cost) {
-                    System.out.println(-1);
-                    return;
+                    dist[edge.to] = dist[edge.from] + edge.cost;
                 }
             }
         }
 
+        // 음수 사이클 확인
+        for (Edge edge : edges) {
+            if (dist[edge.from] != Long.MAX_VALUE &&
+                    dist[edge.to] > dist[edge.from] + edge.cost) {
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        // 결과 출력
         for (int i = 2; i <= cityTotal; i++) {
             if (dist[i] == Long.MAX_VALUE) {
                 System.out.println(-1);

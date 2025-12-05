17c15738b9a95d8e6f949d9a0963c5c5ca533735
JAEIK JEONG <wodlr1207@naver.com>
2025-12-05 15:15:52 +0900

    [Gold I] Title: 구간 합 구하기, Time: 664 ms, Memory: 95832 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     static long[] arr;
+    static int l;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +15,7 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int l = 0; // 2^l >= n을 만족하는 최소의 l
+        // 2^l >= n을 만족하는 최소의 l
         for (int i = 1; i < n; i++) {
             if (Math.pow(2, i) >= n) {
                 l = i;
@@ -36,41 +37,62 @@ public class Main {
         for (int i = 0; i < m + k; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
-            long b = Long.parseLong(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             long c = Long.parseLong(st.nextToken());
 
             if (a == 1) { // b를 c로 교체
-                // 질의 인덱스를 원본 인덱스로 변환
-                int index = (int) (b + Math.pow(2, l) - 1);
-                long different = c - arr[index];
-                while (index > 0) {
-                    arr[index] += different;
-                    index = index / 2;
-                }
-
+                update(b, c);
             } else {  // b부터 c까지 합을 구하여 출력
-                // 질의 인덱스를 원본 인덱스로 변환
-                int start = (int) (b + Math.pow(2, l) - 1);
-                int end = (int) (c + Math.pow(2, l) - 1);
-                long result = 0;
-                while (start <= end) {
-                    if (start % 2 == 1) {
-                        result += arr[start];
-                        start = (start + 1) / 2;
-                    } else {
-                        start = start / 2;
-                    }
-
-                    if (end % 2 == 0) {
-                        result += arr[end];
-                        end = (end - 1) / 2;
-                    } else {
-                        end = end / 2;
-                    }
-
-                }
+                long result = query(b, (int) c);
                 System.out.println(result);
             }
         }
+    }
+
+    private static void update(int index, long newValue) {
+        // 질의 인덱스를 세그먼트 트리의 리프 노드 인덱스로 변환
+        int treeIndex = index + (int) Math.pow(2, l) - 1;
+
+        // 변경될 값과 기존 값의 차이 계산
+        long difference = newValue - arr[treeIndex];
+
+        // 리프 노드의 값을 업데이트
+        while (treeIndex > 0) {
+            arr[treeIndex] += difference;
+            treeIndex = treeIndex / 2;
+        }
+    }
+
+    private static long query(int start, int end) {
+        // 질의 인덱스를 세그먼트 트리의 리프 노드 인덱스로 변환
+        int startNode = start + (int) Math.pow(2, l) - 1;
+        int endNode = end + (int) Math.pow(2, l) - 1;
+
+        long result = 0;
+        while (startNode <= endNode) {
+            // startNode가 오른쪽 자식(홀수 인덱스)인 경우:
+            // 현재 노드의 값(arr[startNode])을 결과에 더하고, 부모의 다음(오른쪽) 자식으로 이동 (startNode + 1) / 2
+            if (startNode % 2 == 1) {
+                result += arr[startNode];
+                startNode = (startNode + 1) / 2;
+            } else {
+                // startNode가 왼쪽 자식(짝수 인덱스)인 경우:
+                // 값은 더하지 않고, 부모로 이동 startNode / 2
+                startNode = startNode / 2;
+            }
+
+            // endNode가 왼쪽 자식(짝수 인덱스)인 경우:
+            // 현재 노드의 값(arr[endNode])을 결과에 더하고, 부모의 이전(왼쪽) 자식으로 이동 (endNode - 1) / 2
+            if (endNode % 2 == 0) {
+                result += arr[endNode];
+                endNode = (endNode - 1) / 2;
+            } else {
+                // endNode가 오른쪽 자식(홀수 인덱스)인 경우:
+                // 값은 더하지 않고, 부모로 이동 endNode / 2
+                endNode = endNode / 2;
+            }
+        }
+
+        return result;
     }
 }

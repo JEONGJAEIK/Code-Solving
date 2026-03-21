7d53cb75387f4b0322dd87a91b50824522c953a4
JAEIK JEONG <wodlr1207@naver.com>
2026-03-21 21:28:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.75 ms, Memory: 98.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String a : participant) {
+            if (map.containsKey(a)) {
+                Integer value = map.get(a);
+                map.put(a, value + 1);
+            } else {
+                map.put(a, 1);
+            }
+        }
+        
+        for (String b : completion) {
+            if (map.containsKey(b)) {
+                Integer value = map.get(b);
+                if (value == 1) {
+                    map.remove(b);
+                } else {
+                    map.put(b, value - 1);
+                }
+            }
+        }
+        
+        String result = map.keySet().iterator().next();
+        return result;
+    }
+}
